ce527c8922da350b9d61445bf871e4792208a60f
Dvontre Coleman <dvontrec@gmail.com>
2018-12-06 12:49:07 -0500

    Initial Commit

---
+++ Hoodie Weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hoodie Weather
+//
+//  Created by Dvontre Coleman on 12/6/18.
+//  Copyright © 2018 Dvontre A Coleman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
